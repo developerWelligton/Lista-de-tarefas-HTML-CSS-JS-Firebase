1fe0aa37d3d83fd6e769a665b6c20ffd9c0c1d1d
Welligton <developerwelligton@gmail.com>
2021-02-25 00:24:05 -0300

    Day 1

---
+++ init.js
@@ -0,0 +1,11 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyACNpYOCILB_MDyKeOnLkUJjT2rH7DHqpE",
+    authDomain: "todo-list-49448.firebaseapp.com",
+    projectId: "todo-list-49448",
+    storageBucket: "todo-list-49448.appspot.com",
+    messagingSenderId: "15313080518",
+    appId: "1:15313080518:web:c74519ab0ff0f5b9f747c7"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
---
+++ js/auth.js
@@ -0,0 +1,91 @@
+ //login
+ 
+ function Login(){
+
+    if(firebase.auth().correntUser){
+        firebase.auth().signOut()
+    } 
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then(()=>{
+            swal.fire({
+                icon: "success",
+                title: "Login realizado com sucesso"
+            }).then(()=>{
+                setTimeout(()=>{
+                    window.location.replace("index.html")
+                },1000)
+            })
+        })
+        .catch((error)=>{
+            const errorCode = error.code
+                switch(errorCode){
+                    case "auth/wrong-password":
+                        swal.fire({
+                            icon:"error",
+                            title: "Senha Inválida."
+                        })
+                        break;
+                    case "auth/invalid-email":
+                        swal.fire({
+                            icon:"error",
+                            title: "Email inválido."
+                        })
+                        break;
+                    case "auth/user-not-found":
+                        swal.fire({
+                            icon:"warning",
+                            title: "Usuário não encontrado.",
+                            text: "Deseja criar esse usuário ?",
+                            shadowCancelButton: true,
+                            cancelButtonText: "Não",
+                            cancelButtonColor:"d33",
+                            confirmButtonText:"sim",
+                            confirmButtonColor:"#3085d6"
+                        }).then((result)=> {
+                            signUp(email,password)
+                        })
+                        break;
+                    default:
+                        swal.fire({
+                            icon:"error",
+                            title: error.message
+                        })
+                }
+        })
+}
+
+//autenticação
+function signUp(email,password){
+    firebase
+    .auth()
+    .createUserWithEmailAndPassword(email,password)
+    .then( ()=> {
+        swal.fire({
+            icon: "sucess",
+            title: "Usuário criado com sucesso"
+        }).then( ()=>{
+            setTimeout(()=>{
+                window.location.replace("index.html")
+            },1000)
+        })
+    }).catch((error)=>{
+        const errorCode = error.code;
+        switch(errorCode){
+            case "auth/weak-password":
+                swal.fire({
+                    icon: "error",
+                    title: "Senha muito fraca"
+                })
+                break;
+                default:
+                    swal.fire({
+                        icon: "error",
+                        title: error.message
+                    })
+        }
+    })
+}
